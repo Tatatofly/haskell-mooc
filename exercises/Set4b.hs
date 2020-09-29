6a87e6fd1b76beb54dc470b43fdf089473d57a7c
Tatu <git@tatu.moe>
2020-09-29 15:34:35 +0300

    Exercises 4b

--- exercises/Set4b.hs
+++ exercises/Set4b.hs
@@ -21,7 +21,10 @@ import Mooc.Todo
 countNothings :: [Maybe a] -> Int
 countNothings xs = foldr countHelper 0 xs
 
-countHelper = todo
+countHelper :: Num a1 => Maybe a2 -> a1 -> a1
+countHelper Nothing x = x + 1
+countHelper y x = x + 0
+
 
 ------------------------------------------------------------------------------
 -- Ex 2: myMaximum with a fold. Just like in the previous exercise,
@@ -35,7 +38,8 @@ myMaximum :: [Int] -> Int
 myMaximum [] = 0
 myMaximum (x:xs) = foldr maxHelper x xs
 
-maxHelper = todo
+maxHelper :: Ord a => a -> a -> a
+maxHelper x y = max x y
 
 ------------------------------------------------------------------------------
 -- Ex 3: compute the sum and length of a list with a fold. Define
@@ -52,8 +56,10 @@ maxHelper = todo
 sumAndLength :: [Double] -> (Double,Int)
 sumAndLength xs = foldr slHelper slStart xs
 
-slStart = todo
-slHelper = todo
+slStart :: (Double, Int)
+slStart = (0.0, 0)
+slHelper :: (Num a, Num b) => a -> (a, b) -> (a, b)
+slHelper x y = ((fst y) + x, (snd y) + 1)
 
 ------------------------------------------------------------------------------
 -- Ex 4: implement concat with with a fold. Define concatHelper and
@@ -67,8 +73,10 @@ slHelper = todo
 myConcat :: [[a]] -> [a]
 myConcat xs = foldr concatHelper concatStart xs
 
-concatStart = todo
-concatHelper = todo
+concatStart :: [a]
+concatStart = []
+concatHelper :: [a] -> [a] -> [a]
+concatHelper x y = x ++ y
 
 ------------------------------------------------------------------------------
 -- Ex 5: get all occurrences of the largest number in a list with a
